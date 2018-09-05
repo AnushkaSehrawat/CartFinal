e902bda22143f6482359685871b488f17cf9b62d
Anushka Sehrawat <asehrawat@tavisca.com>
2018-09-05 13:28:06 +0530

    project added in local branch

---
+++ Cart/Cart/Icard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cart
+{
+    interface Icard:Ipayment
+    {
+
+      
+        void Payment(int cardno, int cv, int expirydate, string cardname);
+  
+    }
+}
